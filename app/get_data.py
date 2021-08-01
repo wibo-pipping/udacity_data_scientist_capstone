e69cae9e191932017a5dc6d4b9cb089b7d8db8c9
Wibo Pipping <wibo@wetransfer.com>
2021-08-01 14:48:51 +0200

    Add function to plot data

--- app/get_data.py
+++ app/get_data.py
@@ -1,6 +1,12 @@
+import json
 import yfinance as yf
 import pandas as pd
 import datetime as dt
+
+import plotly.graph_objects as go
+
+from plotly.utils import PlotlyJSONEncoder
+
 from typing import Optional
 
 DEFAULT_END_DATE = dt.date.today().strftime('%Y-%m-%d')
@@ -8,7 +14,7 @@ DEFAULT_END_DATE = dt.date.today().strftime('%Y-%m-%d')
 
 def get_ticker_data(ticker_symbol: str, start_date: str='2021-01-01', end_date: str=DEFAULT_END_DATE) -> pd.DataFrame:
     """Retrieve the data through the yahoo finance API between start and end dates and return a pandas DataFrame with
-    the adjusted close values per day.
+    the adjusted close values per day. Return the dataframe with the downloaded data
 
     Keyword arguments:
     ticker_symbol -- Valid ticker symbol to retrieve data for
@@ -20,4 +26,21 @@ def get_ticker_data(ticker_symbol: str, start_date: str='2021-01-01', end_date: 
 
     df.columns = df.columns.str.lower().str.replace(' ','_')
 
-    return df[['adj_close']].to_html()
+    return df
+
+
+def create_candle_json(df, ticker_symbol):
+    fig = go.Figure(data=[
+        go.Candlestick(
+            x=df.index,
+            open=df.open,
+            high=df.high,
+            low=df.low,
+            close=df.adj_close,
+            name=ticker_symbol)
+            ]
+        )
+
+    graphJSON = json.dumps(fig, cls=PlotlyJSONEncoder)
+
+    return graphJSON
