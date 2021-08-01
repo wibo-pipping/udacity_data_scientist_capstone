af19c5a001b7079fdb8227d98756f14f31519067
Wibo Pipping <wibo@wetransfer.com>
2021-08-01 14:46:30 +0200

    Update main.py to use Flask instead of fastapi

--- app/main.py
+++ app/main.py
@@ -1,29 +1,38 @@
-from typing import Optional
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template, request, jsonify
+from get_data import get_ticker_data, create_candle_json
 
-from .get_data import get_ticker_data
+app = Flask(__name__)
 
-app = FastAPI()
+#templates = Jinja2Templates(directory="app/templates")
 
-templates = Jinja2Templates(directory="app/templates")
-
-@app.get('/')
-def home(request: Request):
+@app.route('/', methods=["GET"])
+def home():
     """
     Displays the homepage.
     """
-    result = "Type a ticker symbol"
-    return templates.TemplateResponse("home.html", context={'request': request, 'result': result})
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def process_request():
+    ticker_symbol = request.form.get('ticker_symbol', None)
 
 
-@app.post("/")
-def form_post(request: Request, ticker_symbol: str = Form(...)):
-    result = get_ticker_data(ticker_symbol)
+    df = get_ticker_data(ticker_symbol)
+    ticker_table = df.tail(10).loc[:,['adj_close']].T.to_html()
 
-    context = { 'request': request,
-                'result': result,
-                'ticker_symbol': ticker_symbol
-                }
+    graphJSON = create_candle_json(df, ticker_symbol)
+    print(graphJSON[:50])
 
-    return templates.TemplateResponse('home.html', context=context)
+    return render_template('home.html', 
+                           ticker_symbol=ticker_symbol,
+                           ticker_table=ticker_table,
+                           graphJSON=graphJSON
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
