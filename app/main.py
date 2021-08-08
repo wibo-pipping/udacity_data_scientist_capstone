0124e5c6d3bf04df5b3fb5387be6d0fc8c04dabc
Wibo Pipping <wibo@wetransfer.com>
2021-08-08 13:34:43 +0200

    Add a dropdown menu for input over input box

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from get_data import get_ticker_data, create_candle_json, create_bar_chart, create_line_chart
+from get_data import get_ticker_data, generate_line_graph_json
 
 app = Flask(__name__)
 
@@ -8,19 +8,22 @@ def home():
     """
     Displays the homepage.
     """
+    tickers = ['AAPL','GOOG','MSFT']
 
-    return render_template('home.html')
+    return render_template('home.html', ticker_options = tickers)
 
 @app.route('/', methods=['POST'])
 def process_request():
     ticker_symbol = request.form.get('ticker_symbol', None)
-    ticker_symbol = 'AAPL' if not ticker_symbol else ticker_symbol ## temporary
+    ticker_symbol_dropdown = request.form.get('ticker_symbol_dropdown',None)
 
+    if not ticker_symbol:
+        ticker_symbol = ticker_symbol_dropdown
 
     df = get_ticker_data(ticker_symbol)
     ticker_table = df.tail(10).loc[:,['adj_close']].T.to_html()
 
-    line_graphJSON = create_line_chart(df, ticker_symbol)
+    line_graphJSON = generate_line_graph_json(df, ticker_symbol)
 
     return render_template('home.html', 
                            ticker_symbol=ticker_symbol,
