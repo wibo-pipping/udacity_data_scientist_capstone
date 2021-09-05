bf103f2a28f6ea4676d8c5fb96c294942ae9b28a
Wibo Pipping <wibo@wetransfer.com>
2021-09-05 15:14:03 +0200

    Add epoch timestamp to escape plotly bug

--- app/common.py
+++ app/common.py
@@ -1,4 +1,13 @@
 import datetime as dt
 
+DATE_FORMAT = '%Y-%m-%d''
+
+
 def get_today_date_str():
-    return dt.date.today().strftime('%Y-%m-%d')
+    """Return todays date as a string"""
+    return dt.date.today().strftime(DATE_FORMAT)
+
+
+def get_today_date_str():
+    """Return the epoch timestamp in miliseconds"""
+    return dt.datetime.strptime(get_today_date_str(), DATE_FORMAT).timestamp()*1000
