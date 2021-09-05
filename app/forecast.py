0498f08848e3574b0fcc5647c0d72a6b5eb70405
Wibo Pipping <wibo@wetransfer.com>
2021-09-05 12:12:07 +0200

    Add forecast logic

---
+++ app/forecast.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+
+from pandas.core.indexes.numeric import IntegerIndex
+from fbprophet import Prophet
+
+
+def forecast_data(df: pd.DataFrame, ticker_symbol: str, forecast_column: str='adj_close') -> pd.DataFrame:
+    """Get the dataframe with ticker symbol and run the forecast. Retrieves model parameters for the ticker symbol to optimize performance.
+    Use `forecast_column` to select the column to forecast for. Merges the forecasted data with the 
+    """
+
+    index_name = df.index.name
+    df = df.reset_index(drop=False)
+    df = df.rename(columns={index_name:'ds', forecast_column:'y'})
+
+    model_params = get_prophet_params(ticker_symbol)
+
+    forecast = forecast_with_prophet(df[['ds','y']], prophet_params=model_params)
+
+    result = df.merge(forecast, on='ds', how='outer')
+
+    return result
+
+
+def get_prophet_params(ticker_symbol: str) -> dict:
+    """Read the parameters for prophet from disk and select the params for the current ticker"""
+    
+    with open('models/prophet_params.json','r') as f:
+        all_params = json.load(f)
+    
+    prophet_params = all_params.get(ticker_symbol, {})
+
+    return prophet_params
+
+
+def forecast_with_prophet(df: pd.DataFrame, days: int=180, prophet_params:dict = {}) -> pd.DataFrame:
+    """Use prophet to create a model, fit the data and forecast"""
+
+    model = Prophet(**prophet_params)
+    model.fit(df)
+    future_df = model.make_future_dataframe(periods=days)
+    forecast = model.predict(future_df)
+
+    return forecast
