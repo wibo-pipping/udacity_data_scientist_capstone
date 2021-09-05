44cf716b819f36c8f50d6028d8b3ef94cd7e5328
Wibo Pipping <wibo@wetransfer.com>
2021-09-05 15:20:07 +0200

    Fix: update function name

--- app/common.py
+++ app/common.py
@@ -8,7 +8,7 @@ def get_today_date_str():
     return dt.date.today().strftime(DATE_FORMAT)
 
 
-def get_today_date_str():
+def get_today_epoch():
     """Return the epoch timestamp in miliseconds"""
     # Use get_today_date to get specific timepoint in the day
     return dt.datetime.strptime(get_today_date_str(), DATE_FORMAT).timestamp()*1000
